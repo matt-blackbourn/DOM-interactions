822ca29232a7d87fb82065d8a435ba2236a980b6
matt-blackbourn <matt.blackie114@gmail.com>
2020-09-10 13:22:02 +1200

    add dot hide function

--- game.js
+++ game.js
@@ -13,6 +13,7 @@ function bindEventListeners (dots) {
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
     dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', hide)
   }
 }
 
@@ -30,6 +31,11 @@ function makeBlue(e){
 }
 
 // CREATE FUNCTION hide HERE
+
+function hide(e){
+   e.target.classList.toggle("invisible")
+   updateCounts()
+}
 
 function updateCounts () {
   var totals = {
